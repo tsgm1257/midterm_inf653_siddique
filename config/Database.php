9d4542b53c2bdc843362882a8d4d661958af1574
tsgm1257 <neaz1257@gmail.com>
2025-03-24 22:25:30 -0500

    Added .htaccess file, .gitignore file, and modified Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,24 +1,36 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $port = '5432';
-    private $db_name = 'quotesdb';
-    private $username = 'postgres';
-    private $password = 'postgres';
     private $conn;
+    private $host;
+    private $port;
+    private $dbname;
+    private $username;
+    private $password;
+    
+    public function __construct() {
+        $this->username = getenv('USERNAME');
+        $this->password = getenv('PASSWORD');
+        $this->dbname = getenv('DBNAME');
+        $this->host = getenv('HOST');
+        $this->port = getenv('PORT');
+
+    }
 
     public function getConnection() {
-        $this->conn = null;
-        $dsn = 'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name;
+        if ($this->conn) {
+            return $this->conn;
+        } else {
+            $dsn = "pqsql:host=$this->host;port=$this->port;dbname=$this->dbname";
 
-        try {
-            $this->conn = new PDO($dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
+            try {
+                $this->conn = new PDO($dsn, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->conn;
+            } catch (PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
         }
 
-        return $this->conn;
     }
 }
 ?>
